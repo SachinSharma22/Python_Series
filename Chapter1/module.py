5eb10dadf5bcc3eeb9538ed94189db257bd462b1
SachinSharma22 <s830sharma@gmail.com>
2025-08-01 16:15:38 +0530

    install pyjoke module

---
+++ Chapter1/module.py
@@ -0,0 +1,11 @@
+import pyjokes
+
+# This is a single line comment
+# This jokes comes from pyjokes module
+print("Printing Jokes....")
+joke = pyjokes.get_joke()
+print(joke)
+"""
+This is multi
+line comment in python
+"""
