2e6f3e626d8788aeacfda611913fe857400b9aa1
SachinSharma22 <s830sharma@gmail.com>
2025-08-02 08:56:41 +0530

    input in python

---
+++ Chapter2/Input.py
@@ -0,0 +1,39 @@
+#  Check the type of variable assigned using input () function.
+# a = input("Enter a number")
+# print(type(a))  #It always take value as a string
+# We can convert this str value to integer value
+
+# Write a python program to add two numbers
+# a = int(input("Enter a value "))
+# b = int(input("Enter second value "))
+
+# print(type(a+b),(a+b))
+
+# Write a python program to find reminder when a number is divided by z
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# print("Reminder is: ", a%b)
+
+# Use comparison operator to find out whether a given variable a is greater than b or not. Take a = 34 and b = 80
+
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# if a > b :
+#     print("A is greater than B")
+# elif a == 0 or b == 0:
+#     print("eitehr a or b is zero")
+# else:
+#     print("B is greater than A")
+
+# Write a python program to find an average to two numbers entered by the user.
+# a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+
+# print("Average of two number is: ", (a+b)/2)
+
+# Write a python program to calculate the square of two number entered by the user
+a = int(input("Enter first number: "))
+# b = int(input("Enter second number: "))
+print("The square is : ",a*a)
